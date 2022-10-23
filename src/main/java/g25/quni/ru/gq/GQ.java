4c4146962432409ff71fd6306113c68f8418a250
Ge0rg25 <Ge0rgiusGus@yandex.ru>
2022-10-23 20:34:58 +0300

    First Commit

---
+++ src/main/java/g25/quni/ru/gq/GQ.java
@@ -0,0 +1,17 @@
+package g25.quni.ru.gq;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GQ extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
