b823d39bbd3d12df791462b1a825c9b1c97b96c4
Ge0rg25 <Ge0rgiusGus@yandex.ru>
2022-10-23 20:49:03 +0300

    Update Structure

--- src/main/java/g25/quni/ru/gq/GQ.java
+++ src/main/java/g25/quni/ru/gq/GQ.java
@@ -7,13 +7,11 @@ public final class GQ extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-        System.out.println("Привет зайчики! Timon2006 любит вас!");
 
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
-        System.out.println("Идите нафиг тупые");
     }
 }
