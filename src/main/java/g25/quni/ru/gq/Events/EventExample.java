1d145f377e8c8a43a15d933ffccf068235cf00c9
Ge0rg25 <Ge0rgiusGus@yandex.ru>
2022-10-23 22:11:11 +0300

    Update

--- src/main/java/g25/quni/ru/gq/Events/EventExample.java
+++ src/main/java/g25/quni/ru/gq/Events/EventExample.java
@@ -7,6 +7,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import g25.quni.ru.gq.Misc.MiscExample;
+import java.util.Random;
 
 public class EventExample implements Listener {
     public EventExample(GQ plugin){
@@ -16,6 +17,10 @@ public class EventExample implements Listener {
     @EventHandler
     public void onEvent(PlayerJoinEvent event){
         Player player = event.getPlayer();
+        Random randomizer = new Random();
+        String[] hi_msg = new String[]{"Поприветствуем", "Встречайте", "К нам с ноги залетел", "К нам запрыгнул"};
+        int choice = randomizer.nextInt(hi_msg.length);
+        Bukkit.broadcastMessage(hi_msg[choice] + " " + player.getName());
         player.sendMessage("Приветик!");
         MiscExample.miscMethod(player);
     }
