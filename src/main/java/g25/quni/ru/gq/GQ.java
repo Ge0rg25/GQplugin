9254644d0fa69a21119aa3d37c97da3bb156c329
Quni1337 <galiorcus14@yandex.ru>
2022-10-23 20:47:27 +0300

    Ya gey

--- src/main/java/g25/quni/ru/gq/GQ.java
+++ src/main/java/g25/quni/ru/gq/GQ.java
@@ -7,7 +7,7 @@ public final class GQ extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-        System.out.println("Привет зайчики!");
+        System.out.println("Привет зайчики! Timon2006 любит вас!");
 
     }
 
