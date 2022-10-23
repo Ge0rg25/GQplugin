eec3d276500a70e913c0322434faa62ac0986fae
Ge0rg25 <Ge0rgiusGus@yandex.ru>
2022-10-23 20:40:47 +0300

    update

--- src/main/java/g25/quni/ru/gq/GQ.java
+++ src/main/java/g25/quni/ru/gq/GQ.java
@@ -7,11 +7,13 @@ public final class GQ extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+        System.out.println("Привет зайчики!");
 
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        System.out.println("Идите нафиг тупые");
     }
 }
